f3a4fdc8a08d54d818061b013b3fd33c2f23ffd6
Young Yu <youngyu19@gmail.com>
2024-01-13 17:13:48 -0800

    remove unwanted files

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,3 @@ import { NestableDataTypes } from "./enums";
 
 /* Export EnvVarConfig and Nestable Data Types */
 export { SimpleAppConfig as Config, NestableDataTypes as DataTypes };
-
-/* Run configuration upon import from dependent module */
-// SimpleAppConfig.configure();
-process.env.CONFIG_PATH = 'test2.json'
-// SimpleAppConfig.configure();
-// const map: Map<string, Map<unknown, unknown>> = SimpleAppConfig.get('var2');
-// console.log(map.get('test'));
-
-console.log(SimpleAppConfig.get('var3'));
