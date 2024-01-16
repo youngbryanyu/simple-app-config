a3103d08990f514770b5faf7c451876071a7997f
Young Yu <youngyu19@gmail.com>
2024-01-16 12:10:30 -0800

    1.0.0 - Create initial prod-ready release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,4 @@
 # Changelog
+
+## 1.0.0
+Release initial production-ready version of the library
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { Config } from './simple-app-config';
+import { NestableDataTypes } from './constants';
+import EnvParser from './utils/envParser';
 
 /* Set exports */
-export { Config };
+export { Config, NestableDataTypes as DataTypes, EnvParser };
 
 /* Set Config to be the default export */
 export default Config;
